f39e5d02c5142b2c5add5247cb3a1d90e78f3dc3
Vitez, Radko <Radko.Vitez@2ring.com>
2019-03-01 14:48:38 +0100

    add glob lib

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,47 @@
 'use strict';
 import * as vscode from 'vscode';
 import Handlers from './handlers';
+import glob = require('glob');
 
 const handlers = new Handlers();
 
 export function activate(context: vscode.ExtensionContext) {
+    const s = {
+        'Clients': {
+            childrens: {
+                'Dialog': {
+                    scripts: {
+                        'fiis': ''
+                    }
+                }
+            },
+            scripts: {
+                'build': ''
+            },
+            fsPath: ''
+        }
+    };
+    let sss: any = {};
+    const sa = vscode.workspace.workspaceFolders || [];
+    if (sa) {
+        sa.forEach(element => {
+            glob('**/package.json', {
+                cwd: element.uri.fsPath,
+                ignore: 'node_modules/**'
+            }, (a, items) => {
+                items.forEach((val) => {
+                    if (!sss[element.name]) {
+                        sss[element.name] = {};
+                    }
+                    sss[element.name] = Object.assign({}, val.split('/'));
+                });
+                debugger;
+            });
+        });
+    }
+
+
+
     context.subscriptions.push(vscode.commands.registerCommand('fle.getLayout', handlers.invokeGetLayout));
     context.subscriptions.push(vscode.commands.registerCommand('fle.loadConfiguredLayout', handlers.invokeLoadConfiguredLayout));
     context.subscriptions.push(vscode.commands.registerCommand('fle.setLayout', handlers.invokeSetLayout));
